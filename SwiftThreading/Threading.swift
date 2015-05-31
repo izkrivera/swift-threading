1d84ef2bffe4f8b2e13cf2316fbdabf7c1b68894
Isaac Rivera <developer@isaacrivera.com>
2015-05-31 09:44:53 -0400

    Overloaded marshal operator to handle binary tuple returns.

--- SwiftThreading/Threading.swift
+++ SwiftThreading/Threading.swift
@@ -45,5 +45,37 @@ func ~> <R> (
     }
 }
 
+/**
+
+Contributed by Isaac Rivera on 5/31/2015
+
+Executes the lefthand closure on a background thread accepting a tuple (R, T)
+return and upon completion, the righthand closure on the main thread is executed,
+passing the tuple to the main closure.
+
+Example:
+
+func getData(req: NSURLRequest) -> (result: [String], error: NSError?) {...}
+func storeResult(result: [String], forError error: NSError?)) {...}
+
+{
+getData(req)
+} ~> {
+storeResult($0, forError: $1)
+}
+
+*/
+func ~> <R, T> (
+	backgroundClosure: () -> (R, T),
+	mainClosure:       ((R, T)) -> ())
+{
+	dispatch_async(queue) {
+		let (a, b) = backgroundClosure()
+		dispatch_async(dispatch_get_main_queue(), {
+			mainClosure(a, b)
+		})
+	}
+}
+
 /** Serial dispatch queue used by the ~> operator. */
 private let queue = dispatch_queue_create("serial-worker", DISPATCH_QUEUE_SERIAL)
--- SwiftThreading/ViewController.swift
+++ SwiftThreading/ViewController.swift
@@ -10,26 +10,50 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    @IBAction func handleButton(sender: AnyObject)
-    {
-        // NOTE: The semicolons prevent a compiler error
-        // as of Xcode 6 Beta 3.
-        
-        { log("hello") } ~> { log("goodbye") };
-        
-        { addRange(0..<10_000) } ~> { log("sum = \($0)") };
-    }
+	@IBAction func handleButton(sender: AnyObject)
+	{
+		// NOTE: The semicolons prevent a compiler error
+		// as of Xcode 6 Beta 3.
+		
+		{ log("hello") } ~> { log("goodbye") };
+		
+		{ addRange(0..<10_000) } ~> { log("sum = \($0)") };
+	}
+	
+	@IBAction func handleButtonWithTuple(sender: AnyObject)
+	{
+		{
+			addRangeWithMessage(0..<1_000_000)
+			} ~> {
+				alert($1, "sum = \($0)")
+		}
+	}
 }
 
 func addRange(range: Range<Int>) -> Int
 {
-    log("adding...")
-    return reduce(range, 0, +)
+	log("adding...")
+	return reduce(range, 0, +)
 }
 
 func log(message: String)
 {
-    let main = NSThread.currentThread().isMainThread
-    let name = main ? "[main]" : "[back]"
-    println("\(name) \(message)")
+	let main = NSThread.currentThread().isMainThread
+	let name = main ? "[main]" : "[back]"
+	println("\(name) \(message)")
+}
+
+
+func addRangeWithMessage(range: Range<Int>) -> (Int, String)
+{
+	log("adding...")
+	return (reduce(range, 0, +), "Success!")
+}
+
+func alert(title: String, message:String)
+{
+	let main = NSThread.currentThread().isMainThread
+	let name = main ? "[main]" : "[back]"
+	
+	UIAlertView(title: title, message: "\(name) \(message)", delegate: nil, cancelButtonTitle: "OK").show()
 }
